a926aeb09bd68ffde7055bb670b288c8a7ca2eda
pallsv227 <pallig1@gmail.com>
2017-11-22 14:09:50 +0000

    verkefni 8

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from beaker.middleware import SessionMiddleware
 from sys import argv
 #Bryngeir Ari & Páll Gunnar
 
-@route('/hello/<name>')
+@route('/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 run(host='0.0.0.0', port=argv[1], debug=True, reloader=True, app=app)  # keyrir server
-#HALLO
