ca34ed0638a0b119713befc1a419a75bd02c26d3
pallsv227 <pallig1@gmail.com>
2017-11-22 14:24:37 +0000

    verkefni 8

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from beaker.middleware import SessionMiddleware
 from sys import argv
 #Bryngeir Ari & Páll Gunnar
 
-@route('/')
-def index():
+@route('/hello/<name>')
+def index(name):
     return template('''
-    <b>Palli er kominn á heroku</b>!')
+    <b>Hello {{name}}</b>!', name=name
     ''')
 
 run(host='0.0.0.0', port=argv[1])  # keyrir server
